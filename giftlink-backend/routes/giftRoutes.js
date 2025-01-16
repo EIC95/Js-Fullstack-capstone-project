20ce5ba28a3bb92e26b24ee3c3e57f0e0400bf54
IBRAHIMA CISSÉ <ibou9543@gmail.com>
2025-01-16 04:25:37 +0000

    Update giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 8 */
-const connectToDatabase = require('../models/db.js')
-const express = require('express')
-const router = express.Router()
+const connectToDatabase = require('../models/db.js');
+const express = require('express');
+const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
@@ -22,11 +22,11 @@ router.get('/:id', async (req, res) => {
     try {
         const db = await connectToDatabase();
 
-        const collection = db.collections("gifts")
+        const collection = db.collections("gifts");
 
         const id = req.params.id;
 
-        const gift = await collection.findOne({'id' : id})
+        const gift = await collection.findOne({'id' : id});
 
         if (!gift) {
             return res.status(404).send('Gift not found');
